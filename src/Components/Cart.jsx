d6595fc1dbf56d973aa9cd68b958483e45bf29c3
yirano <yirano@protonmail.ch>
2020-05-03 23:29:11 -0400

    Style and render order in Cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default function Cart() {
+const StyledCartContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+const StyledCard = styled.div`
+
+`
+
+export default function Cart({ order }) {
+  console.log("Cart -> order", order)
   return (
-    <div>
-      Cart
-    </div>
+    <StyledCartContainer>
+      {order.length !== 0 ?
+        <>
+          <h2>Congratulations! Your order is on the way!</h2>
+          {order.map(o => {
+            return (
+              <StyledCard>
+                <p>Pizza Size: {o.size}</p>
+                <p>Toppings Selected: {o.toppingsChecked.map(toppings => toppings.name)} </p>
+              </StyledCard>
+            )
+          })}
+        </>
+        : <h2>Your Cart is empty!</h2>}
+    </StyledCartContainer>
   )
 }
